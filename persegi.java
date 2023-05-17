da8da993fc013934aa3843f66494e554967ddd47
Hafiz Sukmono <74044681+Hafiz-sukmono@users.noreply.github.com>
2023-05-17 19:47:39 +0700

    Tugas OODB Pertemuan 5

--- persegi.java
+++ persegi.java
@@ -1,30 +1,32 @@
-package latihan1;
+package persegi;
+
+/*
+ * NIM  : 1913000018
+ * Nama : Hafiz Sukmono
+ */
+
+// class segiempat merupakan template untuk membuat object
 
 class segiempat {
 	int pjg;
 	int lbr;
 }
-
+	
 public class persegi {
-
+	String name = "Hafiz Sukmono";
+	int bilangan;
+	float berat;
+	char huruf = 'A';
+	boolean status;
+	
 	public static void main(String[] args) {
+		System.out.println("					Koleksi Sepeda                     ");
+		System.out.println("No 	Merk 		Seri 		TglBeli 	   Status 	 Harga");
+		System.out.println("1 	United 		S001 		20-01-2019 	   bekas 	 5.000.000");
+		System.out.println("2 	3sixty 		T092 		28-01-2020 	   Baru 	 9.000.000");
+		System.out.println("3 	Fixie 		F263 		18-03-2015	   bekas 	 3.000.000");
+		System.out.println("4 	Surly 		SL34 		05-5-2020 	   Baru 	12.000.000");
+		System.out.println("5 	Brompton 	B113 		11-12-2019 	   Baru		17.000.000");
 		
-		segiempat segipanjang = new segiempat();
-		segipanjang.pjg = 10;
-		segipanjang.lbr = 5;
-		int luas;
-		int keliling;
-		luas = segipanjang.pjg * segipanjang.lbr;
-		System.out.println("luas segipanjang : "+luas);
-		segiempat bujursangkar = new segiempat();
-		bujursangkar.pjg = bujursangkar.lbr = 5;
-		luas = bujursangkar.pjg * bujursangkar.lbr;
-		System.out.println("Luas bujursangkar : "+luas);
-		keliling = segipanjang.pjg + segipanjang.lbr;
-		System.out.println("keliling segipanjang : "+keliling);
-		keliling = bujursangkar.pjg + bujursangkar.lbr;
-		System.out.println("keliling bujursangkar : "+keliling);
-
 	}
-
 }
